990b3b4c37d92e2cd4b3278900d8b450ac83cb4a
AkashSalodkar <72863220+AkashSalodkar@users.noreply.github.com>
2025-09-13 22:56:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "tomcat_server" {
-  ami           = "ami-02d26659fd82cf299"
+  ami           = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
   security_groups = ["demo-database-rds"]  # Reference the existing security group
   key_name      = "mujahed"
@@ -12,4 +12,5 @@ resource "aws_instance" "tomcat_server" {
 output  "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
 }
+
 
