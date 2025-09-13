b792fbade09077be95bc78f347612c6c920971e8
AkashSalodkar <72863220+AkashSalodkar@users.noreply.github.com>
2025-09-13 22:02:34 +0530

    Update main.tf
    NA

--- main.tf
+++ main.tf
@@ -2,23 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "tomcat_server" {
-  ami           = "ami-084568db4383264d4"
+  ami           = "ami-02d26659fd82cf299"
   instance_type = "t2.micro"
-  security_groups = ["mysg"]  # Reference the existing security group
-  key_name      = "mujahed"
-}
-
-resource "aws_instance" "mysql_server" {
-  ami           = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  security_groups = ["mysg"]  # Reference the existing security group
-  key_name      = "mujahed"
-} 
-
-resource "aws_instance" "maven_server" {
-  ami           = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  security_groups = ["mysg"]  # Reference the existing security group
+  security_groups = ["demo-database-rds"]  # Reference the existing security group
   key_name      = "mujahed"
 }
 
@@ -26,9 +12,4 @@ resource "aws_instance" "maven_server" {
 output  "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
 }
-output  "mysql_server_ip" {
-  value = aws_instance.mysql_server.public_ip
-}
-output  "maven_server_ip" {
-  value = aws_instance.maven_server.public_ip
-}
+
