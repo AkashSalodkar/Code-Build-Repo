972b87648c6d8c666deefae5addd253b098beb90
AkashSalodkar <72863220+AkashSalodkar@users.noreply.github.com>
2025-10-16 12:28:40 +0530

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "tomcat_server" {
   ami           = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   security_groups = ["sgalltraffic"]  # Reference the existing security group
   key_name      = "mujahed"
 }
@@ -12,6 +12,7 @@ resource "aws_instance" "tomcat_server" {
 output  "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
 }
+
 
 
 
